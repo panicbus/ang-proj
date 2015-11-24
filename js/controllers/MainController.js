a755124b1d9412c9910e1fc8aa913dc7389ac61a
panicbus <elementalair@gmail.com>
2015-11-24 00:12:38 -0800

    correcting ng-repeat targets

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,8 @@
 app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {
   forecast.success(function(data) {
+  	// debugger
+  	// console.log("data: " + data.forecastday);
+  	// console.log("forecast: " + forecast.forecastday);
     $scope.tenDay = data;
   });
 }]);
--- js/services/forecast.js
+++ js/services/forecast.js
@@ -1,10 +1,12 @@
 app.factory('forecast', ['$http', function($http) {
-  // return $http.get('http://api.wunderground.com/api/2e91cf72317737fc/forecast10day/q/CA/San_Francisco.json')
-  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
+  return $http.get('http://api.wunderground.com/api/2e91cf72317737fc/forecast10day/q/CA/San_Francisco.json')
+  // return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/forecast-api/forecast.json')
     .success(function(data) {
+    	// debugger
       return data;
     })
     .error(function(err) {
       return err;
 		});
 }]);
+
