51ce9a6b8c26adf2942089617406a04b4f888651
panicbus <elementalair@gmail.com>
2016-01-01 21:14:06 -0800

    adding the clock

--- js/app.js
+++ js/app.js
@@ -1,1 +1,25 @@
 var app = angular.module('ForecastApp', []);
+
+function startTime(){
+	var today, h, m, s;
+	today = new Date();
+	h = today.getHours() % 12 || 12;
+	m = today.getMinutes();
+	s = today.getSeconds();
+
+	h = checkTime(h);
+	m = checkTime(m);
+	s = checkTime(s);
+
+	document.getElementById('clock').innerHTML = h + " " + m + " " + s;
+	var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+
+
+
